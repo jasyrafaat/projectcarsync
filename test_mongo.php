10faf86fa7b4c6f80a8ea91b945e49d2a211b70c
jasyrafaat <145940664+jasyrafaat@users.noreply.github.com>
2025-03-26 04:25:38 +0200

    Add files via upload

--- test_mongo.php
+++ test_mongo.php
@@ -1,16 +1,7 @@
 <?php
-require 'vendor/autoload.php'; // تحميل مكتبة MongoDB
-
-use MongoDB\Client;
-
-try {
-    $mongoClient = new Client("mongodb+srv://jasyrafaat:jasy2002@cluster0.ng0is.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-
-    $database = $mongoClient->selectDatabase('carsynce');
-    $collection = $database->selectCollection('sensors');
-
-    echo "✅ Successfully connected to MongoDB Atlas!";
-} catch (Exception $e) {
-    echo "❌ Connection failed: " . $e->getMessage();
+if (class_exists("MongoDB\Driver\Manager")) {
+    echo "MongoDB extension is loaded!";
+} else {
+    echo "MongoDB extension is NOT loaded!";
 }
 ?>
